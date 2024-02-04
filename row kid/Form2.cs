cbd1ce688b6ea0c2b5ff84ccec39159ef3204942
ida92014 <147349862+ida92014@users.noreply.github.com>
2024-02-04 13:49:30 -0800

    Add files via upload

---
+++ row kid/Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace row_kid
+{
+    public partial class Form2 : Form
+    {
+        int player1 = 0;
+        int movex = 3;
+        int movey = 3;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                pictureBox1.Left -= 5;
+            }
+            if (e.KeyCode == Keys.B)
+            {
+                pictureBox1.Left += 5;
+            }
+            
+
+
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    }
+
